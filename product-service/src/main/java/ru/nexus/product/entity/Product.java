a8de9973fe09bad20c13b448fc462da36ba3e39c
Tim Ter <timter32@gmail.com>
2026-01-30 13:21:06 +0400

    Add: Entity Product

---
+++ product-service/src/main/java/ru/nexus/product/entity/Product.java
@@ -0,0 +1,26 @@
+package ru.nexus.product.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Data
+@Builder
+@Document(collection = "catalog")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    private String id;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+    private String category;
+    private Map<String, Object> attributes;
+}
