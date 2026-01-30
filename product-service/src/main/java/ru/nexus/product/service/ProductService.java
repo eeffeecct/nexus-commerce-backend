89df7b47e5e9ca14a3cc2c64877cf19b8f451396
Tim Ter <timter32@gmail.com>
2026-01-30 13:22:56 +0400

    Add: ProductService (create and update operations using Request and Response methods)

---
+++ product-service/src/main/java/ru/nexus/product/service/ProductService.java
@@ -0,0 +1,57 @@
+package ru.nexus.product.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.nexus.product.dto.ProductRequest;
+import ru.nexus.product.dto.ProductResponse;
+import ru.nexus.product.entity.Product;
+import ru.nexus.product.repository.ProductRepository;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    // CRUD operations
+
+    private final ProductRepository repository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        if (productRequest == null) {
+            throw new IllegalArgumentException("ProductRequest cannot be null");
+            //TODO: Custom Exceptions + Exception Handler
+        }
+        Product product = Product.builder()
+                .title(productRequest.getTitle())
+                .price(productRequest.getPrice())
+                .quantity(productRequest.getQuantity())
+                .category(productRequest.getCategory())
+                .attributes(productRequest.getAttributes())
+                .build();
+        Product savedProduct = repository.save(product);
+        return mapToResponse(savedProduct);
+    }
+
+    public ProductResponse updateProduct(String id, ProductRequest productRequest) {
+        Product product = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+
+        product.setTitle(productRequest.getTitle());
+        product.setPrice(productRequest.getPrice());
+        product.setQuantity(productRequest.getQuantity());
+        product.setCategory(productRequest.getCategory());
+        product.setAttributes(productRequest.getAttributes());
+
+        Product updatedProduct = repository.save(product);
+        return mapToResponse(updatedProduct);
+    }
+
+    private ProductResponse mapToResponse(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getTitle(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getCategory(),
+                product.getAttributes()
+        );
+    }
+}
