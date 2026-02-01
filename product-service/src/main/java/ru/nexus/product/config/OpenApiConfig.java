c355153a511dea4790a3269de38e98bca2bc2d12
Tim Ter <timter32@gmail.com>
2026-02-02 00:17:23 +0400

    update: add OpenAPI

---
+++ product-service/src/main/java/ru/nexus/product/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package ru.nexus.product.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Nexus Product Service API")
+                        .version("1.0")
+                        .description("API for managing product catalog in Nexus Commerce platform"));
+    }
+}
