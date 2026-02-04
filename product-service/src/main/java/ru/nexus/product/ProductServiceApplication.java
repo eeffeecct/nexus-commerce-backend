c269dfc8e2f0313279afb02ae6787a71cfb6facd
Tim Ter <timter32@gmail.com>
2026-02-04 13:37:41 +0400

    refactor: pagination update for new version of Spring

--- product-service/src/main/java/ru/nexus/product/ProductServiceApplication.java
+++ product-service/src/main/java/ru/nexus/product/ProductServiceApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableCaching
 @EnableMongoAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
