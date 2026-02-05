92971314858c93266c66a25c1b0850346e124c20
Tim Ter <timter32@gmail.com>
2026-02-05 20:11:59 +0400

    update: Event to DB

---
+++ inventory-service/src/main/java/ru/nexus/inventory/entity/Inventory.java
@@ -0,0 +1,23 @@
+package ru.nexus.inventory.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "t_inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sku_code", nullable = false, unique = true)
+    private String skuCode;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
