e1ddba97c18deabb51e98418209379c497c6da9e
Tim Ter <timter32@gmail.com>
2026-02-09 18:46:59 +0300

    refactor: refactor DTO`s

--- inventory-service/src/main/java/ru/nexus/inventory/dto/InventoryRequest.java
+++ inventory-service/src/main/java/ru/nexus/inventory/dto/InventoryRequest.java
@@ -1,5 +1,7 @@
 package ru.nexus.inventory.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +11,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class InventoryResponse {
+public class InventoryRequest {
+    @NotBlank(message = "SKU code is required")
     private String skuCode;
+
+    @Min(value = 0, message = "Quantity cannot be negative")
     private Integer quantity;
-    private boolean isInStock;
+
     private Integer version;
 }
