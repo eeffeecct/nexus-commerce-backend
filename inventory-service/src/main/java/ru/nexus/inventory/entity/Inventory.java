bea3ea5842daf9945e173d62369a3dc7db19916b
Tim Ter <timter32@gmail.com>
2026-02-09 15:55:57 +0300

    update: add version for optimistic blocking

--- inventory-service/src/main/java/ru/nexus/inventory/entity/Inventory.java
+++ inventory-service/src/main/java/ru/nexus/inventory/entity/Inventory.java
@@ -1,23 +1,15 @@
 package ru.nexus.inventory.entity;
 
-import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Table(name = "t_inventory")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Inventory {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "sku_code", nullable = false, unique = true)
     private String skuCode;
-
-    @Column(nullable = false)
     private Integer quantity;
+    private Integer version;
 }
